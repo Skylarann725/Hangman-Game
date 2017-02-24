0fc8d47ab5f741f23cb6cc7324a30f00a90c71c0
Skylar Siagel <skylarsiagel@Skylars-MacBook-Pro.local>
2017-02-24 11:04:25 -0500

    finished reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,82 +27,98 @@ window.onload = function() {
     var showLives = document.getElementById("mylives");
     var lettersGuessed = document.getElementById("guessed");
     var showWins = document.getElementById("wins");
-    var resetGame = document.getElementById("reset");
+    // var resetGame = document.getElementById("reset");
 
+    startGame = function() {
+        //Choose random words
+        var correctWord = words[Math.floor(Math.random() * words.length)];
+        console.log(correctWord)
 
-
-
-    //Choose random words
-    var correctWord = words[Math.floor(Math.random() * words.length)];
-    console.log(correctWord)
-
-    //Function to take word and make underscores
-    for (i = 0; i < correctWord.length; i++) {
-        if (correctWord[i] !== " ") {
-            dashes.push(" _ ")
-        } else {
-            dashes.push('-')
-        }
-    }
-
-    console.log(dashes);
-    document.getElementById('actualWord').innerHTML = dashes.join(' ');
-
-
-    //key.event for user input- use key codes
-    document.onkeyup = function(event) {
-        var userGuess = event.key;
-        console.log(userGuess)
-
-
-        // if userguess has been guessed already then alert that they already guessed it
-        if (guessedletters.indexOf(userGuess) == -1) {
-            guessedletters.push(userGuess);
-            console.log(guessedletters)
-        } else {
-            console.log("does exist", userGuess)
-            alert("You already guessed that letter.");
-            return
-        }
-        document.getElementById('guessed').innerHTML = guessedletters.join();
-
-        //if letter is not in the word, then lives go down
-        if (correctWord.indexOf(userGuess) == -1) {
-            lives--;
-        }
-
-        showLives.innerHTML = "Number of Guesses Remaining: " + lives;
-
-        //if lives is less than 1, then game is over
-        if (lives < 1) {
-            showLives.innerHTML = "Game Over";
-            resetGame ();
-        }
-
-        //if letter guessed is correct then add to correct word
-        for (var i = 0; i < correctWord.length; i++) {
-            if (userGuess === correctWord[i]) {
-                dashes[i] = userGuess;
+        //Function to take word and make underscores
+        for (i = 0; i < correctWord.length; i++) {
+            if (correctWord[i] !== " ") {
+                dashes.push(" _ ")
+            } else {
+                dashes.push('-')
             }
         }
 
-        //if you guess all the letters correctly then you win
-        document.getElementById('actualWord').innerHTML = dashes.join('');
-        console.log(correctWord, dashes.join('').replace('-', ' '))
-        if (correctWord === dashes.join('').replace('-', ' ')) {
-            alert("You win!");
-            wins++;
-            resetGame();
+        console.log(dashes);
+        document.getElementById('actualWord').innerHTML = dashes.join(' ');
+
+        //key.event for user input- use key codes
+        document.onkeyup = function(event) {
+            var userGuess = event.key;
+            // if (event.keyCode > 64 && event.keyCode < 91) { //Google this.
+            //     alert("That is not a valid letter!")
+            // }
+            console.log(userGuess)
+
+            // if userguess has been guessed already then alert that they already guessed it
+            if (guessedletters.indexOf(userGuess) == -1) {
+                guessedletters.push(userGuess);
+                console.log(guessedletters)
+            } else {
+                console.log("does exist", userGuess)
+                alert("You already guessed that letter.");
+                return
+            }
+            document.getElementById('guessed').innerHTML = guessedletters.join();
+
+            //if letter is not in the word, then lives go downp
+            if (correctWord.indexOf(userGuess) == -1) {
+                lives--;
+            }
+
+            showLives.innerHTML = "Number of Guesses Remaining: " + lives;
+
+            //if lives is less than 1, then game is over
+            if (lives < 1) {
+                showLives.innerHTML = "Game Over";
+                wins = 0;
+                resetGame();
+            }
+
+
+            //if letter guessed is correct then add to correct word
+            for (var i = 0; i < correctWord.length; i++) {
+                if (userGuess === correctWord[i]) {
+                    dashes[i] = userGuess;
+                }
+            }
+
+            //if you guess all the letters correctly then you win
+            document.getElementById('actualWord').innerHTML = dashes.join('');
+            console.log(correctWord, dashes.join('').replace('-', ' '))
+            if (correctWord === dashes.join('').replace('-', ' ')) {
+                console.log("You win!");
+                wins++;
+                resetGame();
+            }
+
+            showWins.innerHTML = "Wins: " + wins;
+
         }
-
-        showWins.innerHTML = "Wins: " + wins;
-
     }
+    startGame();
 
     //reset the game
     resetGame = function() {
-        window.onload ();
+        lives = 10;
+        dashes = [];
+        guessessofar = "";
+        guessedletters = [];
+
+        showLives = document.getElementById("mylives");
+        lettersGuessed = document.getElementById("guessed");
+        showWins = document.getElementById("wins");
+
+        showLives.innerHTML = "Number of Guesses Remaining: " + lives;
+        showWins.innerHTML = "Wins: " + wins;
+        document.getElementById('guessed').innerHTML = guessedletters.join();
+        // resetGame = document.getElementById("reset");
+        startGame();
 
     }
-}
 
+}
